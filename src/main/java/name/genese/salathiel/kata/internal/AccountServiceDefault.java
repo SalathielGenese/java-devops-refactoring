a66c551a6ae17a6a17fcd62cc240e03596a15db3
Salathiel <salathiel@genese.name>
2025-01-17 19:04:15 +0100

    feat: implement validation
    Signed-off-by: Salathiel <salathiel@genese.name>

--- src/main/java/name/genese/salathiel/kata/internal/AccountServiceDefault.java
+++ src/main/java/name/genese/salathiel/kata/internal/AccountServiceDefault.java
@@ -3,12 +3,18 @@ package name.genese.salathiel.kata.internal;
 import name.genese.salathiel.kata.AccountService;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public final class AccountServiceDefault implements AccountService {
     private BigDecimal balance;
 
-    public AccountServiceDefault(BigDecimal balance) {
-        this.balance = balance;
+    public AccountServiceDefault(BigDecimal initial) {
+        Objects.requireNonNull(initial, "balance is null");
+        if (initial.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("negative balance");
+        }
+
+        this.balance = initial;
     }
 
     public AccountServiceDefault() {
@@ -17,11 +23,17 @@ public final class AccountServiceDefault implements AccountService {
 
     @Override
     public void deposit(double amount) {
+        if (amount <= 0)
+            throw new IllegalArgumentException("negative amount");
         balance = balance.add(BigDecimal.valueOf(amount));
     }
 
     @Override
     public void withdraw(double amount) {
+        if (amount <= 0)
+            throw new IllegalArgumentException("negative amount");
+        if (0 < BigDecimal.valueOf(amount).compareTo(balance))
+            throw new IllegalArgumentException("insufficient balance");
         balance = balance.subtract(BigDecimal.valueOf(amount));
     }
 
